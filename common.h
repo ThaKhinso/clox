e9ddf0812f9cb322f29870135ed35694c7504103
soethiha <usersoe123@gmail.com>
2025-09-03 18:18:45 +0630

    Add project files.

---
+++ common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+
+
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "common.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello, World!\n");
+	getc(stdin);
+	return 0;
+}	
