71762cb5afef61741a8a1d56136ddcf21c63a419
soethiha <usersoe123@gmail.com>
2025-09-03 19:16:38 +0630

    WRITING DYNAMIC ARRAY

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "common.h"
 #include <stdio.h>
+#include "chunk.h"
 
 int main(int argc, char* argv[]) {
-	printf("Hello, World!\n");
-	getc(stdin);
+
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+	freeChunk(&chunk);
+	int out = getc(stdin);
 	return 0;
 }	
